1ec6aff6a3a6d51232252171116e66c8e8ded1aa
Anthony Molina <antonio-molina@att.net>
2019-05-18 12:18:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-colors = ["r", "o", "y", "g", "b", "brown", "indego", "violet"]
+colors = ["r", "o", "y", "g", "b", "b", "i", "v"]
 
 puts "b: blue[0], b: brown[1], g: green[2], i: indego[3], o: orenge[4], r: red[5], v: violet[6], y: yellow[7]"

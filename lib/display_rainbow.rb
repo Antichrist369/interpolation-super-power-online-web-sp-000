5ec1b520bf51a827a3546045f3e38fefd5986500
Anthony Molina <antonio-molina@att.net>
2019-05-18 11:56:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-display_rainbow = ["red", "orenge", "yellow", "grenn", "brown", "indego", "violet"]
+colors = ["red", "orenge", "yellow", "grenn", "brown", "indego", "violet"]
 
 puts "letters: #{brown[4]}, letters: #{green[3]}, letters: #{indego[5]}, letters: #{orenge[1]}, letters: #{red[0]}, letters: #{violet[6]}, letters: #{yellow[2]}"

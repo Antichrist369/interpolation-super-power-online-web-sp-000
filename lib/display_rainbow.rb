ce92c576ffdb6150e2d3c3031886d1e740970e04
Anthony Molina <antonio-molina@att.net>
2019-05-18 12:56:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-  puts "colors: blue[0[, green[1], indego[2], orenge[3], red[4], violet[5], yellow[6]"
+  puts "colors: blue[0], green[1], indego[2], orenge[3], red[4], violet[5], yellow[6]"
 end
